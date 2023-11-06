760ef92c5ce331263a074efa477b8585483eef09
hsdanield <hsdanield@gmail.com>
2023-11-06 18:37:43 -0300

    Adicionando .sh de iniciacao

--- prod/client-connector-base/signal-setup.sh
+++ prod/client-connector-base/signal-setup.sh
@@ -3,6 +3,6 @@ docker cp ./client-connector/connectors_template/ connect-client:/kafka/
 sleep 8
 
 echo "Arquivo de configuracao signal >> cliente1-orclcdb-conn"
-docker exec connect-client /bin/bash -c "curl -X PUT -H 'Content-Type:application/json' http://localhost:8083/connectors/cliente1-orclcdb-conn/config -d @/kafka/connectors_template/oracle.json"
+docker exec connect-client /bin/bash -c "curl -X PUT -H 'Content-Type:application/json' http://localhost:8083/connectors/cliente1-orclcdb-conn/config -d @/kafka/connectors_template/oracle_signal.json"
 
 echo "Agora Rodar o Insert na tabela debezium_signal"
--- prod/client-connector-base/start.sh
+++
@@ -1,9 +0,0 @@
-docker compose down && \
-docker compose up --build --no-start
-docker compose up -d
-sleep 10
-
-docker exec connect-client /bin/bash -c "curl -X PUT -H 'Content-Type:application/json' http://localhost:8083/connectors/cliente1-orclcdb-conn/config -d @/kafka/connectors_template/oracle.json"
-sleep 5
-docker exec connect-client /bin/bash -c "curl -X PUT -H 'Content-Type:application/json' http://localhost:8083/connectors/cliente2-testeDB-conn/config -d @/kafka/connectors_template/mssql.json"
-echo "\n"
